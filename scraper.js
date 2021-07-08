c47ea609c2e76cd7d2f83e1e0a2add0f13b19a34
Ivan <78632369+Ivan-MLN@users.noreply.github.com>
2021-07-08 16:38:49 +0700

    Add files via upload

---
+++ scraper.js
@@ -0,0 +1,38 @@
+const { 
+ tiktok,
+ tiktokmusic,
+ ghstalk,
+ telegra,
+ yta,
+ ytv,
+ emoji,
+ mediafire,
+ covid,
+ tahta,
+ styleText,
+ asmaul,
+ fb,
+ igstalk,
+ twitter,
+ playstore,
+ tebakgambar,
+ 
+} = require('./dl.js');
+
+module.exports.tiktok = tiktok
+module.exports.tiktokmusic = tiktokmusic
+module.exports.ghstalk = ghstalk
+module.exports.telegra = telegra
+module.exports.yta = yta
+module.exports.ytv = ytv
+module.exports.emoji = emoji
+module.exports.mediafire = mediafire
+module.exports.covid = covid
+module.exports.tahta = tahta
+module.exports.styleText = styleText
+module.exports.asmaul = asmaul
+module.exports.fb = fb
+module.exports.igstalk = igstalk
+module.exports.tebakgambar = tebakgambar
+module.exports.playstore = playstore
+module.exports.twitter = twitter
